df727ecfa52405cca6f16b21e3ff606d81104733
Jorvis <jorvis@waggunet.com>
2026-04-25 23:07:27 +0900

    feat: fix getStaticPaths with absolute path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
 });
